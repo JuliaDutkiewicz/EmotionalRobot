d82bb77ca94cd32c554a00feefcfe911d39e6ad2
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2019-12-05 01:18:14 +0100

    Stop EmotionService on application closing

--- app/src/main/java/pl/edu/agh/emotionalrobot/MainActivity.java
+++ app/src/main/java/pl/edu/agh/emotionalrobot/MainActivity.java
@@ -3,6 +3,8 @@ package pl.edu.agh.emotionalrobot;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import pl.edu.agh.emotionalrobot.recognizers.video.VideoEmotionRecognizer;
 
@@ -16,8 +18,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button closeButton = (Button) findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopEmotionService();
+                finish();
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
         Intent intent = new Intent(getApplicationContext(), EmotionService.class);
         getApplicationContext().startService(intent);
+    }
+
+    private void stopEmotionService() {
+        getApplicationContext().stopService(new Intent(getApplicationContext(), EmotionService.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopEmotionService();
     }
 }
 
